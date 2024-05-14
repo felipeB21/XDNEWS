106e80a4c414996f6ce22e9a4237f5ffa15ded7a
felipeB21 <bolgarfelipe@gmail.com>
2024-05-14 01:24:21 -0300

    Added astro auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import auth from "auth-astro";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), auth()],
+  output: "server",
+  adapter: vercel(),
 });
