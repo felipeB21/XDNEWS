3ded6ae9b81310631a738611728e143b32dd9c44
felipeB21 <bolgarfelipe@gmail.com>
2024-05-14 01:24:40 -0300

    Added news page article

---
+++ src/consts/News.ts
@@ -0,0 +1,19 @@
+import { type Article } from "../types/Article";
+
+export const NEWS: Article[] = [
+  {
+    id: "all-star-series",
+    title: "All-Stars Series",
+    image: "/all-star-xd.jpg",
+
+    description:
+      "The All-Star Series is gearing up to launch on May 21, as team @TylerTeeP faces off against team @DKarma to compete in an XDefiant competition.",
+  },
+  {
+    id: "xdefiant-preseason",
+    title: "XDefinat Preseason",
+    image: "/preseason.png",
+    description:
+      "The All-Star Series is gearing up to launch on May 21, as team @TylerTeeP faces off against team @DKarma to compete in an XDefiant competition.",
+  },
+];
