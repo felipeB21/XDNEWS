e5e7f93eee3f8dc7e4ed70ae775457d2c51875a0
felipeB21 <bolgarfelipe@gmail.com>
2024-05-15 04:00:48 -0300

    Added some changes

--- src/consts/News.ts
+++ src/consts/News.ts
@@ -16,4 +16,11 @@ export const NEWS: Article[] = [
     description:
       "The All-Star Series is gearing up to launch on May 21, as team @TylerTeeP faces off against team @DKarma to compete in an XDefiant competition.",
   },
+  {
+    id: "best-class",
+    title: "What is the best class in XDefiant?",
+    image: "/best-class.png",
+    description:
+      "We have collected information from the best XDefiant players who have talked about their experience with each of the factions, thanks to that we were able to make a tier list of the best factions.",
+  },
 ];
